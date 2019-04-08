891d03987fd8a946e91682f3869f206c209d0a45
sagar <sagarmopagar@gmail.com>
2019-04-08 15:59:41 +0530

    -Search Button Exists Test Added

--- src/test/java/Tests/TestGoogleSearchPage.java
+++ src/test/java/Tests/TestGoogleSearchPage.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import Driver.InvokeDriver;
@@ -7,14 +8,22 @@ import junit.framework.Assert;
 
 public class TestGoogleSearchPage extends InvokeDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 
 		TestGoogleSearchPage Test = new TestGoogleSearchPage();
 		WebDriver driver = Test.DriverReturner();
 		driver.get("https://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
-
+		
+		String btnSearch = "(.//*[@value='Google Search'])[2]";
+		
+		boolean btnEnabled = driver.findElement(By.xpath(btnSearch)).isEnabled();
+		if	(!btnEnabled)
+		{
+			throw new Exception();
+		}
+		System.out.println("Test case was executed successfully");
 		driver.quit();
 	}
 }
